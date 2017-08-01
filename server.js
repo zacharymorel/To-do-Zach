222c65c4c102c19fc88c33a725eb31ad7991582e
Zachary Morel <morelz133@gmail.com>
2017-07-31 20:41:52 -0400

    This is the commit with Dynamic node.Js is done with the server creating a DOM.

--- server.js
+++ server.js
@@ -1,13 +1,69 @@
-const express = require('express-sessions')
+const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
-const expressSessions = require('express-sessions')
+const expressSession = require('express-session')
 
 const app = express()
 
 app.engine('mustache', mustacheExpress())
-app.set('views', './templates')
-app.set('views engine', 'mustache')
+app.set('views', './views')
+app.set('view engine', 'mustache')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(
+  expressSession({
+    secret: 'han solo',
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
+app.get('/', (request, response) => {
+  const todoList = request.session.todoList || []
+  // console.log(todoList)
+
+ const todoData = {
+   uncompleted: todoList.filter(todo => {
+     return !todo.completed
+   }),
+   completed: todoList.filter(todo => {
+     return todo.completed
+   })
+ }
+  response.render('home', todoData)
+})
+
+app.post('/create', (request, response) => {
+  const todoList = request.session.todoList || []
+  const getListItem = request.body.listItem
+
+  todoList.push({id: todoList.length + 1, completed: false, listItem: getListItem})
+  // console.log(todoList);
+
+  request.session.todoList = todoList
+
+  response.redirect('/')
+})
+
+app.post('/markComplete/:id', (request, response) => {
+  const todoList = request.session.todoList || []
+  const id = parseInt(request.params.id)
+
+  console.log(id);
+  console.log(todoList);
+  console.log(todoList[id-1])
+
+  const todo = todoList.find(todo => todo.id - 1 === id - 1)
+
+  if (todo) {
+    todo.completed = true
+    request.session.todoList
+  }
+  console.log(todo);
+  response.redirect('/')
+})
+app.listen(3000, () => {
+  console.log('may the force be with you')
+})
